36f54024a22911069329dfba268da4e4491760fb
azzzz327 <31851715+azzzz327@users.noreply.github.com>
2017-12-01 16:05:25 +0700

    Update fft.js

--- fft.js
+++ fft.js
@@ -12,11 +12,11 @@ const rp = require('request-promise');
 const _ = require('lodash');
 
 const User = {
-    username: '',
-    password: ''
+    username: 'kx839',
+    password: '081264715402'
 }
 
-const TargetUsername = '';
+const TargetUsername = 'ayutingting92';
 
 const Login = async function(User){
 
